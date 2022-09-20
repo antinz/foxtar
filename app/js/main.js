8a8294f0a166d3c2e31e2ac05237807929dfb0c7
antinz <antoninzhirov@gmail.com>
2022-09-20 17:52:26 +0300

    added 404, contact us, about pages

--- app/js/main.js
+++ app/js/main.js
@@ -34,13 +34,23 @@ $(function () {
 		$('.icon-th-list').removeClass('active');
 	});
 
-	$('.menu__btn').on('click', function(){
+	$('.product-details__tabs .tab').on('click', function (event) {
+		var id = $(this).attr('data-id');
+		$('.product-details__tabs').find('.tab-item').removeClass('active-tab').hide();
+		$('.product-details__tabs .tabs').find('.tab').removeClass('active');
+		$(this).addClass('active');
+		$('#' + id).addClass('active-tab').fadeIn();
+		return false;
+	});
+
+	$('.menu__btn').on('click', function () {
 		$('.menu__list').slideToggle();
 	});
 
-	$('.header__btn-menu').on('click', function(){
+	$('.header__btn-menu').on('click', function () {
 		$('.header__box').toggleClass('active');
 	});
+
 
 	var mixer = mixitup('.products__inner-box');
 });
